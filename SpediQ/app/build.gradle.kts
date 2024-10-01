a0d10b2d7ed455997e12377c439a4eb777673143
Jose David Florez Ruiz <89201134+J0FR@users.noreply.github.com>
2024-10-01 12:23:44 -0500

    Conexion Firebase

--- SpediQ/app/build.gradle.kts
+++ SpediQ/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -50,7 +51,8 @@ android {
 }
 
 dependencies {
-
+    implementation(platform("com.google.firebase:firebase-bom:33.3.0"))
+    implementation("com.google.firebase:firebase-analytics")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
