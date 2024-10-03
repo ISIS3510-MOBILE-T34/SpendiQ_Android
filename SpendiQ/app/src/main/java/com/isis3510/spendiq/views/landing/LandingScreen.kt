b141e3fc7ab69f8c0b9a063f14de357449dc371e
NCKDayz <n.diaz9@uniandes.edu.co>
2024-10-02 19:55:41 -0500

    Bugs Log In Fixed

--- SpendiQ/app/src/main/java/com/isis3510/spendiq/views/landing/LandingScreen.kt
+++ SpendiQ/app/src/main/java/com/isis3510/spendiq/views/landing/LandingScreen.kt
@@ -1,2 +1,31 @@
 package com.isis3510.spendiq.views.landing
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.isis3510.spendiq.views.theme.SpendiQTheme
+
+@Composable
+fun LandingScreen() {
+    Box(
+        modifier = androidx.compose.ui.Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Entro a la app",
+            fontSize = 24.sp
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WelcomeScreenPreview() {
+    SpendiQTheme {
+        LandingScreen()
+    }
+}
