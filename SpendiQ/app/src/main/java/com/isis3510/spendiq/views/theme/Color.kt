af88b83bfde047eb3fabe863fa1749d636b885f8
Juan Diego Ortega <j.ortegam@uniandes.edu.co>
2024-11-12 00:29:14 -0500

    navBar Update

--- SpendiQ/app/src/main/java/com/isis3510/spendiq/views/theme/Color.kt
+++ SpendiQ/app/src/main/java/com/isis3510/spendiq/views/theme/Color.kt
@@ -9,3 +9,22 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val GreenSQ = Color(0xFFB3CB54)
+val PinkSQ = Color(0xFFC33BA5)
+val BlueSQ = Color(0xFF5875DD)
+
+
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+val Gray10 = Color(0xFFFAFAFA)
+val Gray20 = Color(0xFFE0E0E0)
+val Gray30 = Color(0xFFBDBDBD)
+val Gray40 = Color(0xFF9E9E9E)
+val Gray50 = Color(0xFF757575)
+val Gray60 = Color(0xFF616161)
+val Gray70 = Color(0xFF424242)
+val Gray80 = Color(0xFF303030)
+val Gray90 = Color(0xFF212121)
+
