ff28a91bdcb5d908d7dcb3ed40164633bf5fdbd0
Jose David Florez Ruiz <89201134+J0FR@users.noreply.github.com>
2024-11-21 01:53:30 -0500

    Preferences DataStore | AddTransactionModal | IncludeLocation button

--- SpendiQ/app/build.gradle.kts
+++ SpendiQ/app/build.gradle.kts
@@ -118,4 +118,5 @@ dependencies {
     implementation("androidx.room:room-ktx:$roomVersion")
     implementation("androidx.work:work-runtime-ktx:2.8.0")
     implementation("androidx.startup:startup-runtime:1.1.1")
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
 }
