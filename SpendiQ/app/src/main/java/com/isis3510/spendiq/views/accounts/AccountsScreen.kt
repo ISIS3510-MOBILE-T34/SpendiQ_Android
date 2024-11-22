d62c40354da93cb290fc897ffdef04e3e92f0d6c
Jose David Florez Ruiz <89201134+J0FR@users.noreply.github.com>
2024-11-21 22:41:42 -0500

    Nueva Vista

--- SpendiQ/app/src/main/java/com/isis3510/spendiq/views/accounts/AccountsScreen.kt
+++ SpendiQ/app/src/main/java/com/isis3510/spendiq/views/accounts/AccountsScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -74,6 +76,16 @@ fun AccountsScreen(
                 transactionViewModel = transactionViewModel,
                 accountViewModel = accountViewModel
             )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { navController.navigate("mapScreen") }, // Navigate to the Map view
+                modifier = Modifier
+                    .padding(16.dp)
+//                    .align(Alignment.BottomEnd)
+            ) {
+                Icon(imageVector = Icons.Filled.LocationOn, contentDescription = "Go to Map")
+            }
         }
     ) { innerPadding ->
         // Column layout for the main content
